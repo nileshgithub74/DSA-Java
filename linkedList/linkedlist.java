e37c3c965bc8cb4c0c46e7016a4e1db98c696d7f
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2024-06-09 12:34:34 +0000

    commit

---
+++ linkedList/linkedlist.java
@@ -0,0 +1,53 @@
+// package linkedList;
+
+
+// class Node{
+//     int data;
+//     Node next;
+//     public Node(int data){
+//         this.data = data;
+//         next = null;
+//     }
+// }
+// public class linkedlist {
+//     Node head;
+
+
+//     public  void InsertAtEnd( int data){     
+//         Node newNode = new Node(data );
+//         if(head == null){
+//             head = newNode;
+//             return;
+//         }
+        
+//         Node curr = head;
+//         while(curr.next != null){
+//             curr = curr.next;
+
+//         }
+//         curr.next = newNode;
+
+//     }
+//     public  void display()  // to display the element....
+//     {
+//         Node curr = head;
+//         while(curr != null){
+
+//             System.out.println(curr.data);
+//             curr = curr.next;
+
+//         }
+
+//     }
+//     public static void main(String[] args){
+//         linkedlist li = new linkedlist();
+//         li.InsertAtEnd(20);
+//         li.InsertAtEnd( 30);
+//         li.InsertAtEnd( 40);
+//         li.InsertAtEnd( 40);
+
+//         li.display();
+
+
+//     }
+// }

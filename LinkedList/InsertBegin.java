891b5dc245ad530a867001f24a3ce288001dbc3b
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2024-06-09 11:27:40 +0000

    c

---
+++ LinkedList/InsertBegin.java
@@ -0,0 +1,16 @@
+class Node{
+    int data ;
+    Node next;
+    public Node(int data){
+        this.data = data;
+        next = null;
+    }
+
+    
+
+}
+public class InsertBegin{
+    public static  Node insertBegin(Node head, int data){
+        
+    }
+}

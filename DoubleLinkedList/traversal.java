c05b2b7b5fd5f65bc8593b2f488976b933cdc5e5
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2024-06-13 08:16:52 +0000

    comit

--- DoubleLinkedList/traversal.java
+++ DoubleLinkedList/traversal.java
@@ -1,31 +1,31 @@
 
 
-class Node {
-    int data;
-    Node prev;
-    Node next;
-    Node(int data){
-        this.data = data;
-        prev = null;
-        next= null;
-    }
-}
-public class traversal {
+// class Node {
+//     int data;
+//     Node prev;
+//     Node next;
+//     Node(int data){
+//         this.data = data;
+//         prev = null;
+//         next= null;
+//     }
+// }
+// public class traversal {
 
-    void display(Node head){
-        if(head == null)
-           return ;
+//     void display(Node head){
+//         if(head == null)
+//            return ;
         
-        System.out.println(head.data +" ");
-        Node temp = head.next;
-        while(temp != head){
-            System.out.println(temp.data+" ");
-            temp= temp.next;
+//         System.out.println(head.data +" ");
+//         Node temp = head.next;
+//         while(temp != head){
+//             System.out.println(temp.data+" ");
+//             temp= temp.next;
 
-        }
+//         }
 
-    }
+//     }
 
 
     
-}
+// }

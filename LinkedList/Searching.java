8e31cc1b9e8b411ce9d496cb349e43e89591de97
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2024-06-13 08:18:44 +0000

    commit

--- LinkedList/Searching.java
+++ LinkedList/Searching.java
@@ -1,49 +1,49 @@
-class Node{
-    int data;
-    Node next;
-    public Node(int data){
-        this.data = data;
-        this.next = null;
+// class Node{
+//     int data;
+//     Node next;
+//     public Node(int data){
+//         this.data = data;
+//         this.next = null;
 
-    }
+//     }
 
-    static int SearchElement(int element, Node head ){
-        Node curr = head;
-        int position = 1;
-        while(curr != null){
-            if(curr.data == element){
-                return   position;
+//     static int SearchElement(int element, Node head ){
+//         Node curr = head;
+//         int position = 1;
+//         while(curr != null){
+//             if(curr.data == element){
+//                 return   position;
 
-            }else{
-                position ++;
-                curr = curr.next;
-            }
-        }
-        return -1;
-    }
+//             }else{
+//                 position ++;
+//                 curr = curr.next;
+//             }
+//         }
+//         return -1;
+//     }
 
-    static void display(Node head){
-        Node  curr = head;
-        while(curr != null ){
-            System.out.println(curr.data+" ");
-            curr = curr.next;
-        }
-    }
-}
+//     static void display(Node head){
+//         Node  curr = head;
+//         while(curr != null ){
+//             System.out.println(curr.data+" ");
+//             curr = curr.next;
+//         }
+//     }
+// }
 
-public class Searching{
-    public static void main(String[] args){
-        Node head1 = new Node(10);
-        Node temp1 = new Node(20);
-        Node temp2 = new Node( 30);
+// public class Searching{
+//     public static void main(String[] args){
+//         Node head1 = new Node(10);
+//         Node temp1 = new Node(20);
+//         Node temp2 = new Node( 30);
 
-        head1.next= temp1;
-        temp1.next = temp2;
+//         head1.next= temp1;
+//         temp1.next = temp2;
 
       
         
-        // Node.display(head1);
-        int position= Node.SearchElement( 20, head1);
-        System.out.println(position);
-    }
-}
+//         // Node.display(head1);
+//         int position= Node.SearchElement( 20, head1);
+//         System.out.println(position);
+//     }
+// }
--- linkedList/insertAtPosition.java
+++ linkedList/insertAtPosition.java
@@ -1,91 +1,91 @@
-package linkedList;
+// package linkedList;
 
-class Node{
-    int data;
-    Node next;
+// class Node{
+//     int data;
+//     Node next;
 
-    Node( int data){
-        this.data = data;
-    }
-}
+//     Node( int data){
+//         this.data = data;
+//     }
+// }
 
-public class insertAtPosition {
-    Node head;
+// public class insertAtPosition {
+//     Node head;
 
-    public void InsertAtEnd(int data){
-        Node newNode = new Node(data);
-        if(head == null){
-            head= newNode;
-            return;
-        }
-        else{
+//     public void InsertAtEnd(int data){
+//         Node newNode = new Node(data);
+//         if(head == null){
+//             head= newNode;
+//             return;
+//         }
+//         else{
 
-            Node curr = head;
-            while(curr.next != null){
-                curr = curr.next;
-            }
-            curr.next = newNode;
-        }
-    }
-    public void insertFirst(int data){
-        Node temp =new Node(data);
-        if(head == null){
-            head = temp;
-        }else{
-            temp.next = head;
-            head= temp;
-        }
-    }
-    public void insertPosition(int data, int pos){
+//             Node curr = head;
+//             while(curr.next != null){
+//                 curr = curr.next;
+//             }
+//             curr.next = newNode;
+//         }
+//     }
+//     public void insertFirst(int data){
+//         Node temp =new Node(data);
+//         if(head == null){
+//             head = temp;
+//         }else{
+//             temp.next = head;
+//             head= temp;
+//         }
+//     }
+//     public void insertPosition(int data, int pos){
         
-        Node  temp= new Node(data);
-        if(pos == 1){
-            temp.next = head;
-            head= temp;
+//         Node  temp= new Node(data);
+//         if(pos == 1){
+//             temp.next = head;
+//             head= temp;
          
-        }
+//         }
 
-        Node curr = head;
+//         Node curr = head;
 
-        for(int i=1; i< pos-1; i++){
-            curr = curr.next;
-        }
+//         for(int i=1; i< pos-1; i++){
+//             curr = curr.next;
+//         }
 
-        temp.next = curr.next;
-        curr.next = temp;
-
-
-    }
-
-    public void display(){
-        Node curr = head;
-
-        while( curr.next != null){
-            System.out.println(curr.data + " ->");
-            curr = curr.next;
-        }
-
-    }
-
-    public static void main(String[] args){
-        insertAtPosition li = new insertAtPosition();
-        li.InsertAtEnd(10);
-        li.InsertAtEnd(20);
-        li.InsertAtEnd(30);
-        li.InsertAtEnd(40);
+//         temp.next = curr.next;
+//         curr.next = temp;
 
 
-        System.out.println("Element in the linked list are: ");
-        li.display();
+//     }
 
-        li.insertPosition(50, 3);
-        System.out.println("after updating: ");
-        li.display();
+//     public void display(){
+//         Node curr = head;
+
+//         while( curr.next != null){
+//             System.out.println(curr.data + " ->");
+//             curr = curr.next;
+//         }
+
+//     }
+
+//     public static void main(String[] args){
+//         insertAtPosition li = new insertAtPosition();
+//         li.InsertAtEnd(10);
+//         li.InsertAtEnd(20);
+//         li.InsertAtEnd(30);
+//         li.InsertAtEnd(40);
+
+
+//         System.out.println("Element in the linked list are: ");
+//         li.display();
+
+//         li.insertPosition(50, 3);
+//         System.out.println("after updating: ");
+//         li.display();
 
 
 
 
-    }
+//     }
 
 
-}
+// }

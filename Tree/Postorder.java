43373a69ffc2aec667c4865569d09dd2fd2ee385
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2024-06-13 09:10:45 +0000

    commit

--- Tree/Postorder.java
+++ Tree/Postorder.java
@@ -1,38 +1,38 @@
-class  Node{
-    int data;
-    Node left;
-    Node right;
+// class  Node{
+//     int data;
+//     Node left;
+//     Node right;
 
-    Node( int data){
-        this.data = data;
-        this.left = null;
-        this.right = null;
-    }
+//     Node( int data){
+//         this.data = data;
+//         this.left = null;
+//         this.right = null;
+//     }
 
 
-}
+// }
  
-public class Postorder {
+// public class Postorder {
 
-    static void postorder(Node root){
-        if( root != null){
-            postorder(root.left);
-            postorder(root.right);
-            System.out.print(root.data+" ");
-        }
-    }
+//     static void postorder(Node root){
+//         if( root != null){
+//             postorder(root.left);
+//             postorder(root.right);
+//             System.out.print(root.data+" ");
+//         }
+//     }
 
-    public static void main(String[] args){
-        Node root = new Node( 10);
-        root.right = new Node( 20);
-        root.left = new Node( 30);
+//     public static void main(String[] args){
+//         Node root = new Node( 10);
+//         root.right = new Node( 20);
+//         root.left = new Node( 30);
 
-        root.left.right = new Node(40);
-        root.right.left = new Node( 60);
+//         root.left.right = new Node(40);
+//         root.right.left = new Node( 60);
 
-        System.out.print("Postorder Traversal : ");
-        postorder(root);
+//         System.out.print("Postorder Traversal : ");
+//         postorder(root);
 
-    }
+//     }
     
-}
+// }

808e729bda66953b9413d9aabe25bd42bc948fcc
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2024-06-13 08:17:29 +0000

    committ

---
+++ Tree/TreeDemo.java
@@ -0,0 +1,33 @@
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+public class TreeDemo {
+    public static void inorder(Node root) {
+        if (root != null) {
+            inorder(root.left);
+            System.out.print(root.data + " ");
+            inorder(root.right);
+        }
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(10);
+        root.left = new Node(20);
+        root.right = new Node(30);
+        root.right.right = new Node(40);
+        root.left.left = new Node(50);
+
+        System.out.println("Inorder traversal of Tree:");
+        inorder(root);
+        System.out.println(); // New line for better output formatting
+    }
+}

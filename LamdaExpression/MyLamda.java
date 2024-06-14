3a369ea7fbc71ab921d1d27060ab174bbdf545db
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2024-06-14 06:30:52 +0000

    commit

---
+++ LamdaExpression/MyLamda.java
@@ -0,0 +1,44 @@
+package LamdaExpression;
+
+
+@FunctionalInterface
+
+
+interface Mylamda{
+
+    public void display();
+}
+
+
+class Myclass implements Mylamda{
+    public void display(){
+        System.out.println("Heyy , this is nilesh");
+    }
+
+   
+}
+public class Mylamda{
+    public static void main(String[] args){
+        MyLamda m = new MyLamda();
+        m.display();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public class MyLamda {
+    
+}
---
+++ LamdaExpression/MylamdaDemo.java
@@ -0,0 +1,44 @@
+package LamdaExpression;
+
+
+@FunctionalInterface
+
+
+interface Mylamda{
+
+    public void display();
+}
+
+
+class Myclass implements Mylamda{
+    public void display(){
+        System.out.println("Heyy , this is nilesh");
+    }
+
+   
+}
+public class MylamdaDemo{
+    public static void main(String[] args){
+        Mylamda  m = new Myclass();
+        m.display();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public class MyLamda {
+    
+}

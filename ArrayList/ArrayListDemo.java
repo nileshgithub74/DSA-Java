d337c1ae6a874e9827e195db1ff747c3cca139de
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2024-06-15 06:44:54 +0000

    commit

--- ArrayList/ArrayListDemo.java
+++ ArrayList/ArrayListDemo.java
@@ -1,39 +1,39 @@
-// package ArrayList;
-// import java.util.*;
+package ArrayList;
+import java.util.*;
 
-// public class ArrayListDemo {
-//     public static void main(String[] args){
-//         ArrayList<Integer> al = new ArrayList<> ();
-//         ArrayList<Integer> al2 = new ArrayList<> (List.of(20, 30 , 40, 50, 60));
-//         al.add( 20);
-//         al.add(1000);
+public class ArrayListDemo {
+    public static void main(String[] args){
+        ArrayList<Integer> al = new ArrayList<> ();
+        ArrayList<Integer> al2 = new ArrayList<> (List.of(20, 30 , 40, 50, 60));
+        al.add( 20);
+        al.add(1000);
 
-//         al.addAll(2,al2);
-//         // System.out.println(al);
+        al.addAll(2,al2);
+        // System.out.println(al);
 
-//         // System.out.println(al.contains(50));
-//         // System.out.println(al.get(4));
-//         // System.out.println(al.indexOf(50));
+        // System.out.println(al.contains(50));
+        // System.out.println(al.get(4));
+        // System.out.println(al.indexOf(50));
 
 
-//         // for(int i=0 ; i< al.size(); i++){
-//         //     System.out.println(al.get(i));
-//         // }
+        // for(int i=0 ; i< al.size(); i++){
+        //     System.out.println(al.get(i));
+        // }
        
 
-//         // for(Integer x : al){
-//         //     System.out.println(x);
-//         // }
+        // for(Integer x : al){
+        //     System.out.println(x);
+        // }
 
-//         Iterator<Integer>  it = al.iterator();
+        Iterator<Integer>  it = al.iterator();
 
-//         while(it.hasNext()){
-//             System.out.print(it.next()+" ");
-//         }
-
-
+        while(it.hasNext()){
+            System.out.print(it.next()+" ");
+        }
 
 
-//     }
+
+
+    }
     
-// }
+}

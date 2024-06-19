de6e1cba0ea585cfb5e2897ff93facef727a879e
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2024-06-19 05:39:58 +0000

    commit

--- Binarytree/SearchIterative.java
+++ Binarytree/SearchIterative.java
@@ -1,5 +1,20 @@
 package Binarytree;
 
+
+
+class Node{
+    int data;
+    Node right;
+    Node left;
+
+    Node(int data){
+        this.data = data;
+        this.left =null;
+        this.right= null;
+    }
+}
+
 public class SearchIterative {
+    
     
 }

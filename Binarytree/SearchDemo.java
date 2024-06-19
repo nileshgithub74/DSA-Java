2f98bc00c99a4e41206f6c1246dffddb222d669f
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2024-06-19 05:38:03 +0000

    commit

--- Binarytree/SearchDemo.java
+++ Binarytree/SearchDemo.java
@@ -17,8 +17,24 @@ class Node{
 
 
 public class SearchDemo {
+    Node root;
 
-    static void inorder(Node root){
+
+    static boolean search(Node root, int x) {
+        if (root == null) {
+            return false;
+        }
+
+        if (root.data == x) {
+            return true;
+        } else if (root.data < x) {
+            return search(root.right, x);
+        } else {
+            return search(root.left, x);
+        }
+    }
+
+   static  void inorder(Node root){
         if(root != null){
             inorder(root.left);
             System.out.print(root.data+" ");
@@ -26,10 +42,62 @@ public class SearchDemo {
         }
     }
 
+
+    
+
     public static void main(String[] args){
-        Node root = new root(10);
-        root.right
+
+        SearchDemo tree   =new SearchDemo();
+       
+        tree.root = new Node(50);
+
+        
+        tree.root.left = new Node(30);
+        tree.root.right = new Node(70);
+
+        tree.root.left.left = new Node(20);
+        tree.root.left.right = new Node(40);
+
+        tree.root.right.left = new Node(60);
+        tree.root.right.right = new Node(80);
+
+
+     System.out.println("Inoder traversal : ");
+        inorder(tree.root);
+
+        System.out.println();
+
+        boolean result = search(tree.root , 70);
+        if(result){
+            System.out.println("yes it is found");
+        }else{
+            System.out.println(" Not found");
+        }
+        
+    
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
+
+
 
     
     

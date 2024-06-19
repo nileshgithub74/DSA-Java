cb42f1c70b2ecdc96278c295500f54b4954ee8b8
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2024-06-19 05:44:09 +0000

    comit

--- Binarytree/SearchIterative.java
+++ Binarytree/SearchIterative.java
@@ -15,6 +15,31 @@ class Node{
 }
 
 public class SearchIterative {
-    
+    Node root;
+
+    Node insertRec(Node root, int x){
+        Node tem = new Node(x);
+        if(root == null){
+            return tem;
+        }
+        if(root.data == x){
+            return root;
+        }
+        else if( root.data < x){
+            root.right = insertRec(root.right, x);
+        }else{
+            root.left = insertRec(root.left, x);
+        }
+        return root;
+    }
+
+     Node insert(int key){
+        return insertRec(root, key);
+     }
+
+    public static void main(String[] args){
+
+    }
+
     
 }

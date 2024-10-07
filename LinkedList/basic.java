48095ced120abda764b032795ec5637b23ab3973
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2024-10-07 10:26:19 +0000

    java

--- LinkedList/basic.java
+++ LinkedList/basic.java
@@ -1,23 +1,24 @@
+import java.util.*;
+
 class Node {
   int data;
   Node next;
 
-  // Constructor to initialize the node
   Node(int data) {
       this.data = data;
-      this.next = null; // initialize next as null
+       this.next = next;
   }
 }
 
 public class basic {
 
   public static void main(String[] args) {
-      // Create nodes
+
       Node temp = new Node(5);
       Node a = new Node(10);
       Node b = new Node(14);
 
-      // Link the nodes
+
       temp.next = a;
       a.next = b;
 
